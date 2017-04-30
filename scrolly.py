43e5508aaa39ad73ba5257d95b8f3a784e28a2c4
Marco Bakera <marco@bakera.de>
2017-04-30 20:25:19 +0000

    scrolling now controlled by threading and stop event.

--- scrolly.py
+++ scrolly.py
@@ -1,14 +1,17 @@
 import scrollphathd as scroll
+from scrollphathd.fonts import font5x7
 import mosquitto
 import time
 import subprocess
+import threading
 
 class Scrolly:
     def __init__(self, host="cubietruck"):
         self.topic_method = {
             "scrolly/write/scroll": self.write_scroll,            
             "scrolly/write": self.write,
-            "scrolly/power": self.power}
+            "scrolly/power": self.power,
+            "scrolly/brightness": self.set_brightness}
         
         scroll.flip(x=True, y=True)
         scroll.clear()
@@ -22,24 +25,40 @@ class Scrolly:
         self.mos.publish("scrolly/power", 1)
         self.mos.subscribe("scrolly/#")
 
+        self.stop_event = threading.Event()
+        
         self.mos.loop_forever()
 
     def on_message(self, mosq, userdata, msg):
         print("msg received", msg.topic, msg.payload)
 
         if msg.topic in self.topic_method:
+            self.stop_event.set()
             meth = self.topic_method[msg.topic]
             meth(str(msg.payload, encoding="UTF8"))            
 
     def write_scroll(self, message, wait=0.08):
         scroll.clear()
-        scroll.write_string(message, monospaced=True)
-        i = len(message*6)
-        for i in range(i):
+        scroll.write_string(message, x=0, y=0, font=font5x7)
+
+        self.stop_event.clear()
+        th = threading.Thread(target=self._write_scroll_worker,
+                              args=(message, wait))
+        th.start()
+
+    def _write_scroll_worker(self, msg, wait_time):
+        """Method for the worker thread. It is running until the stop event
+        occurs."""
+        
+        while not self.stop_event.is_set():
             scroll.scroll()
             scroll.show()
-            time.sleep(wait)
+            time.sleep(wait_time)
 
+    def set_brightness(self, brightness):        
+        scroll.set_brightness(float(brightness))
+        scroll.show()
+            
     def power(self, on_off):
         if on_off == "0":
             self.write("off")
@@ -53,5 +72,8 @@ class Scrolly:
     def debug(self, msg):
         self.mos.publish("scrolly/debug", msg)
 
-sc = Scrolly()
+
+        
+if __name__ == "__main__":
+    sc = Scrolly()
 
