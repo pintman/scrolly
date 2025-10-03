ca137d9ac10dc65b6d712b1ad30a177cb0f11206
Marco Bakera <marco@bakera.de>
2025-10-03 12:11:01 +0200

    refactor ha_api.py to improve environment variable handling and sensor value retrieval

--- ha_api.py
+++ ha_api.py
@@ -6,21 +6,30 @@ dotenv.load_dotenv()
 
 HASS_URL = os.getenv("HASS_URL", "http://homeassistant.local:8123")  # oder IP-Adresse
 TOKEN = os.getenv("HASS_TOKEN")
-ENTITY_ID = os.getenv("ENTITY_ID")
+ENTITY_ID_STROMVERBRAUCH = os.getenv("ENTITY_ID_STROMVERVERBRAUCH")
+ENTITY_ID_PV = os.getenv("ENTITY_ID_PV")
 
-if not TOKEN or not ENTITY_ID:
-    raise ValueError("HASS_TOKEN und ENTITY_ID müssen gesetzt sein.")
+if not TOKEN or not ENTITY_ID_STROMVERBRAUCH or not ENTITY_ID_PV:
+    raise ValueError("Environment variables missing.")
 
-headers = {
-    "Authorization": f"Bearer {TOKEN}",
-    "Content-Type": "application/json",
-}
+def get_sensor_value(entity_id):
+    headers = {
+        "Authorization": f"Bearer {TOKEN}",
+        "Content-Type": "application/json",
+    }
 
-response = requests.get(f"{HASS_URL}/api/states/{ENTITY_ID}", headers=headers)
+    response = requests.get(f"{HASS_URL}/api/states/{entity_id}", headers=headers)
 
-if response.status_code == 200:
-    data = response.json()
-    value = data["state"]   # hier ist dein Wert als String
-    print(f"Sensorwert: {ENTITY_ID}", value)
-else:
-    print("Fehler:", response.status_code, response.text)
+    if response.status_code == 200:
+        data = response.json()
+        value = data["state"]   # hier ist dein Wert als String
+        return value
+    else:
+        print("Fehler:", response.status_code, response.text)
+
+if __name__ == "__main__":
+    v = get_sensor_value(ENTITY_ID_STROMVERBRAUCH)
+    print("Wert Stromverbrauch:", v)
+    v = get_sensor_value(ENTITY_ID_PV)
+    print("Wert PV:", v)
+
